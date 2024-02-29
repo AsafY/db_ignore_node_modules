783dd49c57a8ab724ecaddf3f1c24783277d1e70
asaf <asaf@yarkoni.net>
2024-02-29 11:33:18 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # db_ignore_node_modules
+```
+A quick & dirty solution to
 set DropBox to recursively ignore all node_modules dirs.
+```
+
+including node_modules in DropBox backup takes unneeded space and causes all sorts of issues with node debugging
+
+Dropbox does not allow you to set a global ignore pattern.
+see:
+https://www.dropboxforum.com/t5/Dropbox-ideas/Add-dropboxignore-directory-to-exclude-folders-without-using/idi-p/5926/page/2#comments
+
+which forces you to set ignore of each file individually:
+```
+Set-Content -Path 'C:\Dropbox\projects\***\node_modules' -Stream com.dropbox.ignored -Value 1
+```
+
+This piece of code allows you to recursively ignore all 1st level node_modules dirs under the DropBox dir.
+
+Usage:
+- Build the project.
+- RUN: node db_ignore_node_modules "__YOUR__DROPBOX__PROJECTS__DIR__ROOT"
---
+++ index.ts
@@ -0,0 +1,61 @@
+import {spawn} from "child_process";
+import path from "path";
+import {readdir, stat as fsStat} from 'fs/promises'
+import assert from "node:assert";
+
+export const x = 1;
+
+const dropboxRoot = process.argv[2];
+assert(dropboxRoot, `dropbox root cannot be null`);
+
+const nodeModules = await listNodeModules(dropboxRoot);
+console.log(`found ${nodeModules.length} "node_modules"`);
+
+for(const dir of nodeModules) {
+    await runTask(`Set-Content -Path '${dir}' -Stream com.dropbox.ignored -Value 1`);
+}
+console.log(`done!`);
+
+function runTask(command: string) {
+    return new Promise((resolve, reject) => {
+        const child = spawn("powershell.exe", [command]);
+        child.stdout.on("data", function (data) {
+            console.log(`Powershell Data: ${data}`);
+        });
+        child.stderr.on("data", function (data) {
+            reject(data);
+        });
+        child.on("exit", function () {
+            console.log(`${command} finished`);
+            resolve(true);
+        });
+
+        child.stdin.end(); //end input
+    });
+}
+
+async function listNodeModules(dir: string): Promise<string[]> {
+    const results: string[] = [];
+
+    if(dir.endsWith('node_modules')){
+        results.push(dir);
+        console.log(`found ${dir}`);
+        return results;
+    }
+
+    const list = await readdir(dir);
+    if (!list.length) {
+        return results;
+    }
+
+    for (let file of list) {
+        file = path.join(dir, file);
+        const stat = await fsStat(file);
+        if (stat && stat.isDirectory()) {
+
+            results.push(...(await listNodeModules(file)));
+        }
+    }
+
+    return results;
+}
